b28a8febd558abf20232f024ac991c18523eacfd
Takuya Fukudome <wheelorder@gmail.com>
2016-10-17 20:47:22 +0900

    reply sticker

--- app.py
+++ app.py
@@ -21,28 +21,39 @@ channelAccessToken = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
-workerQueue = Queue('default', connection=conn)
+defaultQueue = Queue('default', connection=conn)
 
 def verifySignature(signature, requestBody):
     digest = hmac.new(channelSecret, requestBody, hashlib.sha256).digest()
     return signature == base64.b64encode(digest)
 
-def replyMessage(token, message):
+def replyText(token, text):
     headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + channelAccessToken
     }
     data = {
         'replyToken': token,
-        'messages': [
-            {
-                'type': 'text',
-                'text': message
-            }
-        ]
+        'messages': [{'type':'text','text':text}]
     }
     requests.post('https://api.line.me/v2/bot/message/reply', headers=headers, data=json.dumps(data))
-    return
+
+def replySticker(token, packageId, stickerId):
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + channelAccessToken
+    }
+    data = {
+        'replyToken': token,
+        'messages': [{'type':'sticker','packageId':packageId,'stickerId':stickerId}]
+    }
+    requests.post('https://api.line.me/v2/bot/message/reply', headers=headers, data=json.dumps(data))
+
+def replyMessageEvent(replyToken, message):
+    if message['type'] == 'text':
+        defaultQueue.enqueue_call(func=replyText, args=(replyToken, message['text']), timeout=30)
+    elif message['type'] == 'sticker':
+        defaultQueue.enqueue_call(func=replySticker, args=(replyToken, message['packageId'], message['stickerId']), timeout=30)
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
@@ -55,8 +66,7 @@ def webhook():
     for event in data['events']:
         replyToken = event['replyToken']
         if event['type'] == 'message':
-            text = event['message']['text']
-            workerQueue.enqueue_call(func=replyMessage, args=(replyToken, text), timeout=30)
+            replyMessageEvent(event['replyToken'], event['message'])
     return json.dumps('')
 
 if __name__ == "__main__":
